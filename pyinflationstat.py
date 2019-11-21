c58274e8fc4d78ad9a29572b926c6ae80be1b839
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-21 12:09:20 -0500

    [SCRIPT] [WEBDRIVER] [INIT] : setting up the webdriver and opening a test url

--- pyinflationstat.py
+++ pyinflationstat.py
@@ -1,4 +1,29 @@
+from selenium import webdriver
+import traceback
+import sys
+import time
+
+def open_url(driver, url):
+    try:
+        driver.get(url)
+
+    except Exception:
+        traceback.print_exc()
+        driver.quit()
+
+    finally:
+        driver.quit()
+
+def init_driver():
+    try:
+        driver = webdriver.Chrome(executable_path=".//chromedriver")
+        return driver
+    except:
+        print("chromedriver not found, please place it on the root of the project's folder")
+        sys.exit(1)        
+
 def main():
-    print("main")
+    driver = init_driver()
+    open_url(driver, 'https://www.programiz.com/python-programming/time')
 
 main()
